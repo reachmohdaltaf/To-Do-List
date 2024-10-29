d56a5bc644b80774ff0de4ef72f6fa78b694e70f
reachmohdaltaf <reachmohdaltaf@gmail.com>
2024-10-29 20:22:24 +0530

    editbtn

--- script.js
+++ script.js
@@ -23,11 +23,14 @@ const createTask = (task) => {
   const taskcontainer = document.createElement("div");
   const taskElement = document.createElement("p");
   const deletebtn = document.createElement("button");
+  const editbtn = document.createElement('button')
 
   taskcontainer.classList.add("task");
   deletebtn.classList.add("deletebtn");
   deletebtn.innerText = "Delete";
-  
+  editbtn.innerText = "Edit";
+  editbtn.classList.add("editbtn");
+
   
   deletebtn.addEventListener('click', () => {
     const dailytask = JSON.parse(localStorage.getItem("tasks")) || [];
@@ -35,10 +38,26 @@ const createTask = (task) => {
     localStorage.setItem("tasks", JSON.stringify(updatedTasks));
     taskcontainer.remove();
 });
- 
+
+
+editbtn.addEventListener('click', ()=>{
+  let changetask = prompt("Change your task");
+  if(changetask){
+    taskElement.innerText = changetask;
+
+    const dailytask = JSON.parse(localStorage.getItem("tasks")) || [];
+    const updatedTasks = dailytask.map(t => (t === task ? changetask : t));
+    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+  }
+
+
+});
+
+
  
   taskElement.innerText = task;
   taskcontainer.append(taskElement);
+  taskcontainer.append(editbtn);
   taskcontainer.append(deletebtn);
   added.append(taskcontainer);
 };
